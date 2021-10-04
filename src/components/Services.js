ba288362f701cba93a9879fefcbf9ef7eb4d0d6d
calebjsmith7 <calebjoelsmith@gmail.com>
2021-10-04 13:04:24 -0600

    new projects

--- src/components/Services.js
+++ src/components/Services.js
@@ -16,23 +16,32 @@ class Services extends Component {
           
           <div className='sb'>
             <div className='boxes'>
-              <a href="https://worshippro.org">
+            <a href="https://kennedy-84a26.web.app/">
                 <div className='triplebox b1'></div>
               </a>
-              <a href="https://kennedy-84a26.web.app/">
+              <a href="https://wordpress-665021-2176451.cloudwaysapps.com/">
                 <div className='triplebox b2'></div>
               </a>
-              <a href="https://greenhousebotanicals-a7a0b.web.app/">
+              <a href="https://tradersrally.com">
                 <div className='triplebox b3'></div>
               </a>
-              <a href="https://shop.jfc.org">
+              <a href="https://apps.apple.com/us/app/worship-pads-pro/id1586199401">
                 <div className='triplebox b4'></div>
               </a>
-              <a href="https://jfc.org/missions">
+              <a href="https://worshippro.org">
                 <div className='triplebox b5'></div>
               </a>
-              <a href="https://jubilee-3e466.web.app">
+              <a href="https://greenhousebotanicals-a7a0b.web.app/">
                 <div className='triplebox b6'></div>
+              </a>
+              <a href="https://shop.jfc.org">
+                <div className='triplebox b7'></div>
+              </a>
+              <a href="https://jfc.org/missions">
+                <div className='triplebox b8'></div>
+              </a>
+              <a href="https://jubilee-3e466.web.app">
+                <div className='triplebox b9'></div>
               </a>
             </div>
            </div>
